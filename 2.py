7207097ed9c9ffe9d01e90314a78d53c6641c609
maheepmunsi <mridul.munsi@gmail.com>
2026-03-20 21:06:02 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,27 @@
+#creating variables
+a = 5
+b = 10
+
+name= "maheep"
+print(name)
+
+
+
+#performing addition
+c = a + b
+print(c)
+#performing multiplication
+d = a * b
+print(d)
+#performing division
+e = b / a
+
+print(e)
+#performing subtraction
+f = b - a
+
+#user input
+
+name = input("what is your name? ")
+
+print("\nhello", name, "\nwelcome to Python!" )
---
+++ getting started with  python.py
@@ -0,0 +1,15 @@
+#print a sentence
+print("im starting with python today")
+
+#print a number
+print(100)
+
+#next line after the print
+print("this is the next line \n")
+
+
+#pass multiple arguments for printing
+print("40+2, the answer is", 42)
+
+#end argument of print statement
+print("My name is maheep", end=" !")
